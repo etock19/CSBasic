daa15a609420fbc5d5c8638d96a2877eb8e98bdb
github kang <etock19@naver.com>
2024-07-02 19:05:07 +0900

    #2 헬로월드 출력

--- CSBasic_2432/Program.cs
+++ CSBasic_2432/Program.cs
@@ -10,6 +10,18 @@ namespace CSBasic_2432
     {
         static void Main(string[] args)
         {
+            // C#에서도 헬로 월드를?!
+            Console.WriteLine("Hello World?!");
+
+            // 오버플로우 연습
+            int a = 2000000000;  // 20억
+            int b = 1000000000;  // 10억
+            Console.WriteLine(a + b);
+
+            // 오버플로우 연습2
+            Console.WriteLine((long)a + b);
+            Console.WriteLine(a + (long)b);
+            Console.WriteLine((long)a + (long)b);
         }
     }
 }
